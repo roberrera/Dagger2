1724bbd4bfd12ce85a914b06da77e0c609a7be6b
roberrera <roberterrera@gmail.com>
2016-04-25 14:33:08 -0400

    Update MainActivity

--- app/src/main/java/com/roberterrera/dagger2/MainActivity.java
+++ app/src/main/java/com/roberterrera/dagger2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.roberterrera.dagger2;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.roberterrera.dagger2.classes.MyApplication;
+import com.roberterrera.dagger2.classes.NetworkApi;
+
+import javax.inject.Inject;
+
+public class MainActivity extends Activity {
+
+    @Inject
+    NetworkApi networkApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((MyApplication) getApplication()).getComponent().inject(this);
+
+        boolean injected =  networkApi == null ? false : true;
+        TextView userAvailable = (TextView) findViewById(R.id.target);
+        userAvailable.setText("Dependency injection worked: " + String.valueOf(injected));
     }
 }
